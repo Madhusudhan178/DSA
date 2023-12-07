fd168cd5692c4a73dfa7a92243e4c69f55fe03fd
Madhusudhan178 <madhusudhanmayur@gmail.com>
2023-12-08 04:48:59 +0530

    Learning

--- basics-2/a.cpp
+++ basics-2/a.cpp
@@ -1,11 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 int main()
 {
-     
-     string str;
-     getline(cin,str);
-     cout<<str;
-     
+     pair<int, int> p = {1, 3};
+     cout << p.first << " " << p.second;
+
+     pair<int, pair<int, int>> q = {1, {2, 5}};
+     cout << q.first << " " << q.second.first << " " << q.second.second<<endl;
+
+     pair<int,int> arr[]={{1,2},{4,5},{5,6}};
+     cout<<arr[0].first<<arr[1].second;
+
      return 0;
 }
