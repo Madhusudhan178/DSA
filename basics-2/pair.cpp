bc903be6e5802c420320a6bba2bce9ac6c6fe624
Madhusudhan178 <madhusudhanmayur@gmail.com>
2023-12-08 06:29:27 +0530

    Learning

--- basics-2/pair.cpp
+++ basics-2/pair.cpp
@@ -11,7 +11,8 @@ int main()
 
      pair<int,int> arr[]={{1,2},{4,5},{5,6}};
      cout<<arr[0].first<<arr[1].second;
-     
+
+ 
 
      return 0;
 }
--- basics-2/vector.cpp
+++ basics-2/vector.cpp
@@ -19,10 +19,34 @@ int main()
      vector<int> s1(s);
 
      //Access the vectors
-     cout<<arr[0]<<" " <<arr[1]<<" "<<arr[2]<<" "<<arr[6]<< " "<<arr.at(3);
+     cout<<arr[0]<<" " <<arr[1]<<" "<<arr[2]<<" "<<arr[6]<< " "<<arr.at(3)<<endl;
 
+     //Other ways
+    
+     for(vector<int>::iterator z=arr.begin();z != arr.end();z++){
+          cout<<*(z)<<" ";
+     }
+     cout<<endl;
+     for(auto it=arr.begin();it<arr.end();it++){
+          cout<<*(it)<<" ";
+     }
+     cout<<endl;
+     for(auto it:arr){
+          cout<<it<<" ";
+     }
+     cout<<endl;
+     //{10,20,12,23}
+     arr.erase(arr.begin()+1);
+     //{10,20,12,23,25}
+     arr.erase(arr.begin()+2;v.begin+4)//{10,20,35}[start,end]
 
-
+     //insert
+     vector<int>v(2,100);//{100,100}
+     v.insert(v.begin(),300);//{300,100,100}
+     v.insert(v.begin()+1,2,10);//{300,10,10,100,100}
+     //v.size()
+     //v.pop_back();//pop last element
+     //v1.swap(v2);
 
 
 
