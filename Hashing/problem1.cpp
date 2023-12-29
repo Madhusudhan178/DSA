a7c5949ef2fe56e4e3e8a0a5ebad7d21942cead9
Madhusudhan178 <madhusudhanmayur@gmail.com>
2023-12-29 06:05:35 +0530

    learning

--- Hashing/problem1.cpp
+++ Hashing/problem1.cpp
@@ -1,8 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+int fun(int number,int a[])
+{
+     int counter=0;
+     for(int i;i<5,i++)
+     {
+          if(a[i]==number)
+          {
+               counter++;
+          }
+     }
 
+     return counter;
+}
 int main()
 {
+     //HOW MANY IT APPEARS an ELEMENT BRUTE FORCE
+     int number;
+     cout<<"ENTER THE value :"<<endl;
+     cin>>number;
+     int arr[5]={1,2,1,3,2};
+     cout<<"IT apperars "<<fun(number,arr[5])<<"int the given array"<<endl;s
+
 
 
 
