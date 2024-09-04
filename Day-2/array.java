7dc428f9deff967f30c55a80e38d259cacdb1e8b
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-09-04 20:45:11 +0530

    inital commit

---
+++ Day-2/array.java
@@ -0,0 +1,16 @@
+public class array {
+    public static void main(String[] args) {
+        int num[]={10,20,50,44,12};
+        for(int i=0;i<6;i++)
+        {
+            System.out.print(num[i]+",");
+        }
+        // try
+        // {
+
+        // }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("HELLO");
+        // }
+    }
+}
---
+++ Day-2/hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args) {
+        String s="boy";
+        if(s.equals("boy"))
+        {
+            System.out.println("left");
+        }
+        else{
+            System.out.println("right");
+        }
+    }
+}
